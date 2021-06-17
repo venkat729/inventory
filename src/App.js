19c00b184b00b7976e19c84cf65f72aa437b6fcf
Venkatesh Pagadala <pvenkat729@gmail.com>
2021-06-17 21:44:34 +0530

    added fetch method for materials

--- src/App.js
+++ src/App.js
@@ -2,40 +2,66 @@ import React from 'react';
 import { Table, Form, Button, Row, Col } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import { request } from 'http';
 import MaterialStats from './MaterialStats';
 
+const API_URL = 'http://localhost:3333';
 const AddItem = (props) => {
+	function submit() {
+		const input = document.getElementsByClassName('form-inputs');
+		const requestOptions = {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({
+				material_code: input[0].nodeValue,
+				item_description: input[1].nodeValue,
+				total_quantity: input[2].nodeValue,
+				po_no: input[3].nodeValue,
+				remarks: input[4].nodeValue,
+			}),
+		};
+		fetch(`${API_URL}/add_material`,requestOptions).then(res => {
+			res = res.json();
+			if(res.success) {
+				self.fetchMaterials();
+			} else {
+				alert('Some error occured while adding data');
+			}
+		});
+	}
 	return (
 		<div className="d-flex justify-content-center myform">
 			<Form>
 				<Form.Group as={Row} controlId="formHorizantalText">
 					<Form.Label column sm={6}>Material Code</Form.Label>
 					<Col sm={15}>
-						<Form.Control type="text" />
+						<Form.Control className="form-inputs" type="text" />
 					</Col>
 				</Form.Group>
 				<Form.Group as={Row} controlId="formHorizantalText">
 					<Form.Label column sm={6}>Item Description</Form.Label>
 					<Col sm={15}>
-						<Form.Control type="text" />
+						<Form.Control className="form-inputs" type="text" />
 					</Col>
 				</Form.Group>
 				<Form.Group as={Row} controlId="formHorizantalText">
 					<Form.Label column sm={6}>Total Quantity</Form.Label>
 					<Col sm={15}>
-						<Form.Control type="text" />
+						<Form.Control type="text" className="form-inputs"/>
 					</Col>
 				</Form.Group>
 				<Form.Group as={Row} controlId="formHorizantalText">
 					<Form.Label column sm={6}>PO No.</Form.Label>
 					<Col sm={15}>
-						<Form.Control type="text" />
+						<Form.Control type="text" className="form-inputs"/>
 					</Col>
 				</Form.Group>
 				<Form.Group as={Row} controlId="formHorizantalText">
 					<Form.Label column sm={6}>Remarks</Form.Label>
 					<Col sm={15}>
-						<Form.Control type="text" />
+						<Form.Control type="text" className="form-inputs"/>
 					</Col>
 				</Form.Group>
 				<div className="d-flex justify-content-around">
@@ -50,7 +76,7 @@ const AddItem = (props) => {
 						type="button">
 						Back
 					</button>
-					<button className="btn btn-success btn-md" type="button">Submit</button>
+					<button className="btn btn-success btn-md" onClick={() => this.submit()} type="button">Submit</button>
 				</div>
 			</Form>
 		</div>
@@ -118,26 +144,26 @@ export default class App extends React.Component {
 			dashboard: true,
 			addItem: false,
 			updateItem: false,
-			materials: [
-				[1, 'some data', 32, 5632654, 'nothing'],
-				[2, 'some data', 32, 5632654, 'nothing'],
-				[3, 'some data', 32, 5632654, 'nothing'],
-				[4, 'some data', 32, 5632654, 'nothing'],
-				[5, 'some data', 32, 5632654, 'nothing'],
-				[1, 'some data', 32, 5632654, 'nothing'],
-				[1, 'some data', 32, 5632654, 'nothing'],
-				[1, 'some data', 32, 5632654, 'nothing'],
-				[1, 'some data', 32, 5632654, 'nothing'],
-				[1, 'some data', 32, 5632654, 'nothing'],
-				[1, 'some data', 32, 5632654, 'nothing'],
-				[1, 'some data', 32, 5632654, 'nothing'],
-			],
+			materials: [],
 		};
 	}
 	componentDidMount() {
 		// http.request("http://localhost:3333/materials");
 		// get materials data from database
 		// update materials state
+	}
+	fetchMaterials() {
+		fetch(`${API_URL}/materials`,{
+			method: 'GET',
+			headers: {
+				'Content-Type':'application/json'
+			}
+		}).then(res=>res.json())
+		.then(data => {
+			this.setState({
+				materials: data
+			});
+		})
 	}
 	handleBack = () => {
 		this.setState({
